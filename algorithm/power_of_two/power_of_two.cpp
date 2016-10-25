5abbce054daecb06b7884b46cb73db5d3eecef51
AIxi0425 <aixi0425@gmail.com>
2016-10-25 20:33:54 +0800

    add power_of_two

---
+++ algorithm/power_of_two/power_of_two.cpp
@@ -0,0 +1,26 @@
+/*
+ * Given an integer, write a function to determine if it is a power of two.
+ * Time :
+ * Space:
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+	bool Is_power_of_two(int n)
+	{
+		return (n > 0) && (!(n & (n - 1)));
+	}
+};
+
+int main()
+{
+	Solution A;
+	cout << A.Is_power_of_two(1023) << endl;
+	
+	return 0;
+}
+
